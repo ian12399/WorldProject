9ac381e45c23fceff981c5dd030a32df58f062fa
Ian Page <radbradmac@Ians-MacBook-Pro.local>
2016-04-02 16:19:54 -0300

    first commit

---
+++ First Game/Assets/Scripts/BallControl.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var rotationSpeed = 100;
+var jumpHeight = 8;
+
+private var isFalling = false;
+
+function Update () {
+
+//Handele Ball rotation
+var rotation : float = Input.GetAxis("Horizontal") * rotationSpeed;
+rotation *= Time.deltaTime;
+GetComponent.<Rigidbody>().AddRelativeTorque(Vector3.back * rotation);
+
+if(Input.GetKeyDown(KeyCode.W) && isFalling == false){
+
+  GetComponent.<Rigidbody>().velocity.y = jumpHeight;
+  
+}isFalling = true;
+
+}
+
+function OnCollisionStay(){
+
+isFalling = false;
+
+}
